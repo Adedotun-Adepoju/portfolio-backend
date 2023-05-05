36fbe8f1bf8a256c8273ade53600591b51b527bf
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-05-05 23:37:36 +0100

    add needed dtos

---
+++ src/modules/portfolio/dto/create-experience.dto.ts
@@ -0,0 +1,25 @@
+import { IsNotEmpty, IsString, IsOptional, IsArray } from "class-validator";
+
+export class CreateExperienceDto {
+	@IsString()
+	@IsNotEmpty()
+	company: string;
+
+	@IsString()
+	@IsNotEmpty()
+	job_title: string
+
+	@IsNotEmpty()
+	start_date: Date 
+
+	@IsOptional()
+	end_date: Date 
+
+	@IsString()
+	@IsNotEmpty()
+	link: string 
+
+	@IsArray()
+	@IsNotEmpty()
+	roles: string[]
+}
